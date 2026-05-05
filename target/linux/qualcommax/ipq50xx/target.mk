b20c32bc45caf0a170561765a115e97bf3b81f5c
George Moussalem <george.moussalem@outlook.com>
2026-05-05 15:16:32 +0400

    qualcommax: ipq50xx: convert to UNIPHY PCS and DWMAC stack
    Convert to UNIPHY PCS and DWMAC ethernet stack from qca-ssdk.
    
    Signed-off-by: George Moussalem <george.moussalem@outlook.com>

--- target/linux/qualcommax/ipq50xx/target.mk
+++ target/linux/qualcommax/ipq50xx/target.mk
@@ -1,6 +1,6 @@
 SUBTARGET:=ipq50xx
 BOARDNAME:=Qualcomm Atheros IPQ50xx
-DEFAULT_PACKAGES += kmod-qca-nss-dp
+DEFAULT_PACKAGES += kmod-dwmac-ipq5018
 
 define Target/Description
 	Build firmware images for Qualcomm Atheros IPQ50xx based boards.
