e3eda310286f61f4c239c49939dfc5fe54514daf
Robert Marko <robert.marko@sartura.hr>
2026-08-11 12:27:26 +0200

    airoha: an7583: add Datamate NanoDPU
    Add support for the Datamate NanoDPU, including eMMC, I2C, USB, NPU,
    Ethernet and SFP connectivity.
    
    Build matching TF-A and U-Boot firmware with eMMC production boot, DHCP
    and a 64 KiB ubootenv partition. Generate a GPT-based eMMC factory image
    containing BL2, BL31 with U-Boot, the environment and production FIT
    image, along with a FIT sysupgrade image.
    
    Signed-off-by: Robert Marko <robert.marko@sartura.hr>

--- target/linux/airoha/an7583/base-files/lib/upgrade/platform.sh
+++ target/linux/airoha/an7583/base-files/lib/upgrade/platform.sh
@@ -13,6 +13,7 @@ platform_check_image() {
 	local board=$(board_name)
 
 	case "$board" in
+	datamate,nanoDPU|\
 	nokia,xg-040g-mf-ubi)
 		fit_check_image "$1"
 		return $?
@@ -30,11 +31,20 @@ platform_do_upgrade() {
 	local board=$(board_name)
 
 	case "$board" in
+	datamate,nanoDPU|\
 	nokia,xg-040g-mf-ubi)
 		fit_do_upgrade "$1"
 		;;
 	*)
 		nand_do_upgrade "$1"
+		;;
+	esac
+}
+
+platform_copy_config() {
+	case "$(board_name)" in
+	datamate,nanoDPU)
+		emmc_copy_config
 		;;
 	esac
 }
